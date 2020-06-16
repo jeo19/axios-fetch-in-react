54f4f72052bdffa0d375d80dcf93272a81345557
jeo19 <terrakkk@outlook.com>
2020-06-16 23:53:45 +0800

    update:Update the git's Repos list with using Axios module

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import List from './components/List';
@@ -9,9 +10,10 @@ function App() {
     useEffect(() => {
         setAppState({ loading: true });
         const apiUrl = `https://api.github.com/users/hacktivist123/repos`;
-        fetch(apiUrl)
-            .then((res) => res.json())
-            .then((repos) => setAppState({ loading: false, repos: repos }));
+        axios.get(apiUrl).then((repos) => {
+            const allRepos = repos.data;
+            setAppState({ loading: false, repos: allRepos });
+        });
     }, [setAppState]);
     return (
         <div className="App">
